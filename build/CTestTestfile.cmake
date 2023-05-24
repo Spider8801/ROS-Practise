b465487c27126fa2159f28df81b19dd2fee2a393
omeym <omeym24@gmail.com>
2023-05-24 13:25:25 -0700

    ROS Practise

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mm2/Desktop/ROS_trial/src
+# Build directory: /home/mm2/Desktop/ROS_trial/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot_controller")
---
+++ build/my_robot_controller/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mm2/Desktop/ROS_trial/src/my_robot_controller
+# Build directory: /home/mm2/Desktop/ROS_trial/build/my_robot_controller
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
